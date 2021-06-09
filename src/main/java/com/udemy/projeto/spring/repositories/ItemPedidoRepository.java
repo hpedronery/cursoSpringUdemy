f89adb5251f053af3c697be2ca18875a18c5d0a1
hpedronery <hpedronery@gmail.com>
2021-06-09 15:34:09 -0300

    feat(ItemPedido, ItemPedidoPk): Criadas as classes ItemPedido e ItemPedidoPk junto ao repository.

---
+++ src/main/java/com/udemy/projeto/spring/repositories/ItemPedidoRepository.java
@@ -0,0 +1,10 @@
+package com.udemy.projeto.spring.repositories;
+
+import com.udemy.projeto.spring.domain.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer> {
+
+}
