6dbec65efe260485fda56d7b8e434984d0424362
hpedronery <hpedronery@gmail.com>
2021-06-09 10:08:00 -0300

    fix(Produto): Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/udemy/projeto/spring/exceptions/handler/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.udemy.projeto.spring.exceptions.handler;
+
+import com.udemy.projeto.spring.exceptions.ObjectNotFoundException;
+import com.udemy.projeto.spring.exceptions.StandardError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+}
--- src/main/java/com/udemy/projeto/spring/services/impl/CategoriaServiceImpl.java
+++ src/main/java/com/udemy/projeto/spring/services/impl/CategoriaServiceImpl.java
@@ -1,11 +1,14 @@
 package com.udemy.projeto.spring.services.impl;
 
 import com.udemy.projeto.spring.domain.Categoria;
+import com.udemy.projeto.spring.exceptions.ObjectNotFoundException;
 import com.udemy.projeto.spring.repositories.CategoriaRepository;
 import com.udemy.projeto.spring.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.util.Optional;
 
 @Service
 @Validated
@@ -15,8 +18,8 @@ public class CategoriaServiceImpl implements CategoriaService {
     private CategoriaRepository repository;
 
     public Categoria buscar(long id) {
-        Categoria obj = repository.findById(id)
-                .orElse(null);
-        return obj;
+        Optional<Categoria> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
     }
 }
